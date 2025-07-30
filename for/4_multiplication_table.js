f8f3a6609d9e68a27fc645a3f738fe7b70818d9f
khandakershahi <khandakershahi@gmail.com>
2025-07-30 19:20:38 +0600

    for loop task 4 done

---
+++ for/4_multiplication_table.js
@@ -0,0 +1,14 @@
+/***
+
+Generate a multiplication table for number 9
+
+ */
+
+
+/*programming hero*/
+
+let n = 9;
+
+for (let i = 1; i <= 10; i++) {
+    console.log(n + ' X ' + i + ' = ' + n * i);
+}
