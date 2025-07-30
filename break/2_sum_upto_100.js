cb22e359424bec2bd34518d7de57f7ab58bd7d83
khandakershahi <khandakershahi@gmail.com>
2025-07-30 22:51:01 +0600

    break sum 2 done

--- break/2_sum_upto_100.js
+++ break/2_sum_upto_100.js
@@ -1,3 +1,17 @@
 /* 
 Write a while loop that adds numbers starting from 1, but stops (using break) as soon as the sum reaches or exceeds 100
 */
+let i = 1;
+let sum = 0;
+
+while(i <= 200){
+    if(sum > 100){
+        break;
+    }
+
+    console.log(i);
+    sum += i;
+    i++;
+}
+
+console.log('Total sum: ' + sum);
