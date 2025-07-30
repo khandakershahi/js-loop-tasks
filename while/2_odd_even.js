e86f07d0b7c5c09fcc87b4095ece3b2e3f0e1c98
khandakershahi <khandakershahi@gmail.com>
2025-07-30 20:11:23 +0600

    while task 2 done

---
+++ while/2_odd_even.js
@@ -0,0 +1,37 @@
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+console.log('subtak 1');
+let i = 61;
+while(i <= 100){
+    if(i % 2 !== 0){
+        console.log(i);
+    }
+    i++;
+    
+}
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+/*programming hero*/
+
+console.log('subtask 2');
+
+let n = 78;
+while(n <= 98){
+    if(n % 2 === 0){
+        console.log(n);
+    }
+    n++;
+}
